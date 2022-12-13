8ac6dc445da8e33aeba3ccd1946a71597e6558f7
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-12-13 12:16:52 -0300

    feat: find block where element is

--- jump_search.cpp
+++ jump_search.cpp
@@ -2,26 +2,31 @@
 //Source code: https://www.geeksforgeeks.org/jump-search/
 //Time Complexity : O(√n) 
 //Auxiliary Space : O(1)
+//Performance in comparison to linear and binary search:
+//linear search  <  jump search  <  binary search
 
-// C++ program to implement Jump Search
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int jumpSearch(int arr[], int x, int n)
+int jumpSearch(int arr[], int x, int n) //function to find a number in an array through the jumpsearch algorithm. Receives
+//as parameters an array, the size of the array and the number to be found x.
 {
 	// Finding block size to be jumped
-	int step = sqrt(n);
+	int step = sqrt(n); //uses the squareroot function to create the first step, which will define the first block of
+	//indexes to search for the number
 
 	// Finding the block where element is
 	// present (if it is present)
-	int prev = 0;
-	while (arr[min(step, n)-1] < x)
+	int prev = 0; //initialize the previous variable as 0, which represents the first number of the block
+	while (arr[min(step, n)-1] < x) //while the element of the array, from the minimum between step and n is smaller
+	//than x, we still haven't found the right block
 	{
-		prev = step;
-		step += sqrt(n);
-		if (prev >= n)
-			return -1;
+		prev = step; //so go to the next block, we substitute the previous value for the step
+		step += sqrt(n); //and the new step is the last one plus the squareroot of n
+		if (prev >= n) //if the previous number becomes greater than the size of the array, then the number x doesn't
+		//exist in the array
+			return -1; //return -1 because the value doesn't exist
 	}
 
 	// Doing a linear search for x in block
