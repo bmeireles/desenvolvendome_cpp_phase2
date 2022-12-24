8298339c19708421cb186a3d7c0c8e278c4b92ef
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-12-24 13:43:06 -0300

    feat: add fractional knapsack function

--- fractional_snapsack.cpp
+++ fractional_snapsack.cpp
@@ -29,30 +29,29 @@ bool cmp(struct Item a, struct Item b) //function to compare items and sort the 
 }
 
 // Main greedy function to solve problem
-double fractionalKnapsack(int W, struct Item arr[], int N)
+double fractionalKnapsack(int W, struct Item arr[], int N) //function to solve the fractional knapsack problem. Receives
+//as parameters the capacity w, the array of items, and the size of the array
 {
 	// Sorting Item on basis of ratio
-	sort(arr, arr + N, cmp);
+	sort(arr, arr + N, cmp); //call the sort function to sort the array based on the ratio of each item, using the
+    //comparison function
 
-	double finalvalue = 0.0;
+	double finalvalue = 0.0; //we create a variable for the final value as a double because if can be a double
 
 	// Looping through all items
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) { //for every item in the array
 		
-		// If adding Item won't overflow,
-		// add it completely
-		if (arr[i].weight <= W) {
-			W -= arr[i].weight;
-			finalvalue += arr[i].value;
+		// If adding Item won't overflow, add it completely
+		if (arr[i].weight <= W) {  //if the weight of the item is smaller or equal to the total capacity
+			W -= arr[i].weight; //subtract that weight from the total capacity
+			finalvalue += arr[i].value; //and add the value of this item to the final value
 		}
 
-		// If we can't add current Item,
-		// add fractional part of it
-		else {
-			finalvalue
-				+= arr[i].value
-				* ((double)W / (double)arr[i].weight);
-			break;
+		// If we can't add current Item, add fractional part of it
+		else { // if the weight of the item is greater than the total capacity
+			finalvalue += arr[i].value * ((double)W / (double)arr[i].weight); //we add to the final value that item
+            //value, but fractioned, the calculation is value times(current_total_capacity divided by the weight)
+			break; //stop adding beacause this was the last one that could fit the capacity
 		}
 	}
 
@@ -63,12 +62,12 @@ double fractionalKnapsack(int W, struct Item arr[], int N)
 // Driver code
 int main()
 {
-	int W = 50;
-	Item arr[] = { { 60, 10 }, { 100, 20 }, { 120, 30 } };
+	int W = 50; //choose the total capacity
+	Item arr[] = { { 60, 10 }, { 100, 20 }, { 120, 30 } }; //create an array of items
 
-	int N = sizeof(arr) / sizeof(arr[0]);
+	int N = sizeof(arr) / sizeof(arr[0]); //calculate the size of the array
 
 	// Function call
-	cout << fractionalKnapsack(W, arr, N);
+	cout << fractionalKnapsack(W, arr, N); //call the function fractionalKnapsack and return the final value
 	return 0;
 }
